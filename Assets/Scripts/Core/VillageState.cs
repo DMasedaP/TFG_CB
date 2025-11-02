4348d1e557ab62d89174be9deaeefa2e565e37c2
DMasedaP <115708282+DMasedaP@users.noreply.github.com>
2025-11-02 12:04:25 +0100

    SO y basicos
    Creacion de un GameManager, VillageState.asset

---
+++ Assets/Scripts/Core/VillageState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="TFG/VillageState")]
+public class VillageState : ScriptableObject
+{
+    [Header("Food")]
+    public int foodStock;
+    public int foodCapacity;
+
+    [Header("Population")]
+    public int citizens;    // Campesinos
+    public int army;        // Ejercito
+    public int enemies;     // Enemigos
+
+    [Header("Balance")]
+    public float foodPerCitizenPerDay = 1.0f;   // Configurable
+    public float ticksPerDay = 24f;             // 24h del juego
+
+    public int TotalCitizens => citizens + army;
+    public float FoodPerTick => (TotalCitizens * foodPerCitizenPerDay) / Mathf.Max(1f, ticksPerDay);
+}
