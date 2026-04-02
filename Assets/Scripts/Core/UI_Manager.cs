f8a755e75279a7c9946617888fde70fc287c8067
DMasedaP <115708282+DMasedaP@users.noreply.github.com>
2026-04-02 20:03:17 +0200

    Actualizada la UI para los civiles
    Tanto lo visual como la logica del numero mostrado de civiles y su actualización

--- Assets/Scripts/Core/UI_Manager.cs
+++ Assets/Scripts/Core/UI_Manager.cs
@@ -8,6 +8,11 @@ public class UI_Manager : MonoBehaviour
     [SerializeField] TextMeshProUGUI[] resourceTexts; // Los textos de cada recurso
     [SerializeField] Blackboard Blackboard; // Copia del GameManager
 
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(.5f);
+        UpdateResources();
+    }
     public void UpdateResources()
     {
         // FOOD
@@ -18,5 +23,7 @@ public class UI_Manager : MonoBehaviour
         resourceTexts[2].text = Blackboard.village.goldStock.ToString();
         // STONE
         resourceTexts[3].text = Blackboard.village.stoneStock.ToString();
+        // CIVILS
+        resourceTexts[4].text = Blackboard.village.citizens.ToString();
     }
 }
